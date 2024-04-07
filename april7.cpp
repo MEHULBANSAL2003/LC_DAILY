31cc39cf76236aeec5bcf37454f1d953181c8ad1
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-07 08:43:33 +0530

    valid paranthesis string

---
+++ april7.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkValidString(string s) {
+        int low = 0; 
+        int high = 0; 
+
+        for (char c : s) {
+            low += (c == '(') ? 1 : -1;
+            high += (c != ')') ? 1 : -1;
+
+            if (high < 0) break;
+            low = max(low, 0);
+        }
+
+        return low == 0; 
+    }
+
+int main(){
+
+    string s;
+    cin>>s;
+
+
+    bool ans=checkValidString(s);
+    cout<<ans<<endl;
+}
