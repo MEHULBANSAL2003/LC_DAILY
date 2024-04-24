1ae8f0521318f11912a123e1954321b11c2e52c7
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-24 10:50:45 +0530

    nth-tribonacci number

---
+++ april24.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int tribonacci(int n) {
+        vector<int> dp(n+1,-1);
+        if(n<2){
+            return n;
+        }
+
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+       
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+
+        return dp[n];
+    }
+
+int main(){
+    int n;
+    cin>>n;
+
+    int ans=tribonacci(n);
+    cout<<ans<<endl;
+}
